f4c2c4ca5a035d10ede8333a84f79cf85457c326
Lovik-Mahail <87378810+Lovik-Mahail@users.noreply.github.com>
2021-09-26 18:26:52 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+var color="black";
+function onclick(){
+    clearRect(0,0,canvas.width,canvas.height);
+}
+canvas=document.getElementById("mycanvas");
+ctx=canvas.getContext("2d");
+
+function mousedown(e){
+}
